691567370a7b701a25123c567709214257ee097f
d4vves <d4vves@gmail.com>
2020-11-30 18:21:41 -0600

    fixed typo in sentence

--- sentences.js
+++ sentences.js
@@ -1,6 +1,6 @@
 const sentences = [
     'pokemonName',
-    'has anyone run pokemonName in a actionOne build? Curious if it’s viable.',
+    'has anyone run pokemonName in a actionOne build? curious if it’s viable.',
     'holy expletive I can’t believe I found a shiny pokemonName today on route randomInt!',
     '#randomInt pokemonName - many believe pokemonName to be actionOne. If it becomes actionTwo its body has been known to glow.',
     'started a new nuzlocke run and it randomized my starter to pokemonName wtf'
