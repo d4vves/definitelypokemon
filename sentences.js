4cb25db6895ed8e493bf9d26a121c2611867e4a7
d4vves <d4vves@gmail.com>
2020-11-30 19:58:12 -0600

    added sentences

--- sentences.js
+++ sentences.js
@@ -4,8 +4,9 @@ const sentences = [
     'expletive I can’t believe I found a shiny pokemonName today on route randomInt!',
     '#randomInt pokemonName - many believe pokemonName to be actionOne. its body has been known to event.',
     'started a new nuzlocke run and it randomized my starter to pokemonName wtf',
-    'just smoked this dudes pokemonName lol',
-    'just hatched a perfect iv pokemonName bb!'
+    'smoked this dudes pokemonName in battle league lol',
+    'just hatched a perfect iv pokemonName bb!',
+    'omg i pulled a holographic pokemonName out of a pack of cards just now!'
 ];
 
 module.exports = sentences;
