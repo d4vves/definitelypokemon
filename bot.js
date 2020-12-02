34db8a379737db874ed1f9713264e300bb3780a8
d4vves <d4vves@gmail.com>
2020-12-02 15:56:07 -0600

    evaluating repeat tweets

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Twitter = require('twitter');
-const twitterConfig = require('./lib/config');
-const Tweet = new Twitter(twitterConfig);
+const twitterConfig = require('./utils/config');
+const twitter = new Twitter(twitterConfig);
+const params = require('./utils/params');
 const {
     getAnimal,
     getAction,
@@ -16,22 +17,47 @@ const expletiveList = require('./lib/expletives');
 const sentenceList = require('./lib/sentences');
 const eventList = require('./lib/events');
 
-let sentenceStructure = getSentenceStructure(sentenceList);
-let pokemonName = getPokemonName(getAnimal(animalList), getAction(actionList));
-let actionOne = getAction(actionList);
-let actionTwo = getAction(actionList);
-let randomInt = Math.floor(Math.random() * 251);
-let expletive = getExpletive(expletiveList);
-let event = (getEvent(eventList));
-let status = getTweet(sentenceStructure, pokemonName, actionOne, actionTwo, randomInt, expletive, event);
+const getStatus = () => {
+    let sentenceStructure = getSentenceStructure(sentenceList);
+    let pokemonName = getPokemonName(getAnimal(animalList), getAction(actionList));
+    let actionOne = getAction(actionList);
+    let actionTwo = getAction(actionList);
+    let randomInt = Math.floor(Math.random() * 251);
+    let expletive = getExpletive(expletiveList);
+    let event = getEvent(eventList)
+    let status = getTweet(sentenceStructure, pokemonName, actionOne, actionTwo, randomInt, expletive, event);
+    return status;
+};
 
-Tweet.post("statuses/update", { status })
-  .then((res) => {
-    if (!res) return;
-    console.log(res);
-    console.log(status);
-  })
-  .catch((error) => {
-    console.log(`🚦 🚦`, error);
-    throw error;
-  });
+const postStatus = (status) => {
+    twitter.post("statuses/update", { status })
+        .then((res) => {
+            if (!res) return;
+            console.log(res);
+            console.log(status);
+        })
+        .catch((error) => {
+            console.log(`🚦 🚦`, error);
+            throw error;
+        });
+}
+
+twitter.get('statuses/user_timeline', params)
+    .then(res => {
+        let status = getStatus();
+        let prevStatus = res[0].full_text;
+        
+        let firstWord = status.split(' ')[0];
+        let prevFirstWord = prevStatus.split(' ')[0];
+
+        if(firstWord === prevFirstWord) {
+            status = getStatus();
+            postStatus(status)
+        } else {
+            postStatus(status);
+        };
+    })
+    .catch((error) => {
+        console.log(`🚦 🚦`, error);
+        throw error;
+});
---
+++ utils/params.js
@@ -0,0 +1,7 @@
+const params = {
+    screen_name: 'real_pokeman',
+    count: 1,
+    tweet_mode: 'extended'
+};
+
+module.exports = params;
